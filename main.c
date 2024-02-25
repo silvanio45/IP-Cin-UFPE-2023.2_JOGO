c2772b3ccdab171dea67971b0d8d6a9d7786d67e
Pedro Henrique <Henriquesilva200416@gmail.com>
2024-02-25 16:55:54 -0300

    Animações player e Inimigo 1
    animacões jogador: pulo(d/e), pulo com movimento(d/e)
    animações inimigo1: caminhar(d/e), ataque(d/e), morte(d/e)

--- main.c
+++ main.c
@@ -22,10 +22,16 @@
 //----------------------------------------------------------------------------------
 
 Texture2D playerSpriteSheet;
+Texture2D inimigo1SpriteSheet;
+Texture2D inimigo2SpriteSheet;
+Texture2D inimigo3SpriteSheet;
 Texture2D cenario;
 Texture2D cenarioLog;
 Texture2D botaoStart;
 
+//----------------------------------------------------------------------------------
+//  Animacoes
+// Player
 SpriteAnimation playerAnim_walkingRight;
 SpriteAnimation playerAnim_walkingLeft;
 SpriteAnimation playerAnim_idleRight;
@@ -34,6 +40,33 @@ SpriteAnimation playerAnim_idleShotingRight;
 SpriteAnimation playerAnim_idleShotingLeft;
 SpriteAnimation playerAnim_runningShotingRight;
 SpriteAnimation playerAnim_runningShotingLeft;
+SpriteAnimation playerAnim_jumppingMovRight;
+SpriteAnimation playerAnim_jumppingMovLeft;
+SpriteAnimation playerAnim_jumppingRight;
+SpriteAnimation playerAnim_jumppingLeft;
+
+// Inimigo 1
+SpriteAnimation inim1Anim_walkingRight;
+SpriteAnimation inim1Anim_walkingLeft;
+SpriteAnimation inim1Anim_attackingRight;
+SpriteAnimation inim1Anim_attackingLeft;
+SpriteAnimation inim1Anim_dyingRight;
+SpriteAnimation inim1Anim_dyingLeft;
+// Inimigo 2
+SpriteAnimation inim2Anim_walkingRight;
+SpriteAnimation inim2Anim_walkingLeft;
+SpriteAnimation inim2Anim_attackingRight;
+SpriteAnimation inim2Anim_attackingLeft;
+SpriteAnimation inim2Anim_dyingRight;
+SpriteAnimation inim2Anim_dyingLeft;
+// Inimigo 3
+SpriteAnimation inim3Anim_walkingRight;
+SpriteAnimation inim3Anim_walkingLeft;
+SpriteAnimation inim3Anim_attackingRight;
+SpriteAnimation inim3Anim_attackingLeft;
+SpriteAnimation inim3Anim_dyingRight;
+SpriteAnimation inim3Anim_dyingLeft;
+//----------------------------------------------------------------------------------
 
 
 
@@ -65,6 +98,9 @@ int main()
     InitWindow(1280, 720, "Tela Inicial");
 
     playerSpriteSheet = LoadTexture("./resources/Personagem_SpriteSheet.png");
+    inimigo1SpriteSheet = LoadTexture("resources/Inimigo1_SpriteSheet.png");
+    inimigo2SpriteSheet = LoadTexture("resources/Inimigo2_SpriteSheet.png");
+    inimigo3SpriteSheet = LoadTexture("resources/Inimigo3_SpriteSheet.png");
     cenario = LoadTexture("./resources/cenario2.png");
     cenarioLog = LoadTexture("./resources/cenario0.png");
     botaoStart = LoadTexture("./resources/botao2.png");
@@ -72,6 +108,9 @@ int main()
     botaoStartColis = (Rectangle){ BOTAOINICIAL_POS_X, BOTAOINICIAL_POS_Y, botaoStart.width, botaoStart.height };
     playerRect = (Rectangle){PLAYER_POSINICIAL_X, PLAYER_POSINICIAL_Y, PLAYER_DIM_X, PLAYER_DIM_Y};
 
+    //----------------------------------------------------------------------------------
+    //  Animacoes
+    //  Player
     playerAnim_walkingRight = CreateSpriteAnimation(playerSpriteSheet,15,(Rectangle[]){
         (Rectangle){0,  71, 40, 44},
         (Rectangle){40, 71, 40, 44},
@@ -156,6 +195,145 @@ int main()
         (Rectangle){550, 379, 55, 44},
         (Rectangle){605, 379, 55, 44},
     }, 12); 
+    playerAnim_jumppingMovRight = CreateSpriteAnimation(playerSpriteSheet, 10, (Rectangle[]){
+        (Rectangle){0, 423, 40, 44},
+        (Rectangle){40, 423, 40, 44},
+        (Rectangle){80, 423, 40, 44},
+        (Rectangle){120, 423, 40, 44},
+        (Rectangle){160, 423, 40, 44},
+        (Rectangle){200, 423, 40, 44}
+    },6);
+    playerAnim_jumppingMovLeft = CreateSpriteAnimation(playerSpriteSheet, 10, (Rectangle[]){
+        (Rectangle){0, 467, 40, 44},
+        (Rectangle){40, 467, 40, 44},
+        (Rectangle){80, 467, 40, 44},
+        (Rectangle){120, 467, 40, 44},
+        (Rectangle){160, 467, 40, 44},
+        (Rectangle){200, 467, 40, 44}
+    },6);
+    playerAnim_jumppingRight = CreateSpriteAnimation(playerSpriteSheet, 10, (Rectangle[]){
+        (Rectangle){0, 511, 40, 50},
+        (Rectangle){40, 511, 40, 50},
+        (Rectangle){80, 511, 40, 50},
+        (Rectangle){120, 511, 40, 50},
+        (Rectangle){160, 511, 40, 50},
+        (Rectangle){200, 511, 40, 50}
+    },6);
+     playerAnim_jumppingLeft = CreateSpriteAnimation(playerSpriteSheet, 10, (Rectangle[]){
+        (Rectangle){0, 561, 40, 50},
+        (Rectangle){40, 561, 40, 50},
+        (Rectangle){80, 561, 40, 50},
+        (Rectangle){120, 561, 40, 50},
+        (Rectangle){160, 561, 40, 50},
+        (Rectangle){200, 561, 40, 50}
+    },6);
+    //  Inimigo 1
+    inim1Anim_walkingRight = CreateSpriteAnimation(inimigo1SpriteSheet, 15, (Rectangle[]){
+        (Rectangle){575, 843, 55, 47},
+        (Rectangle){509, 843, 55, 47},
+        (Rectangle){444, 843, 55, 47},
+        (Rectangle){379, 843, 55, 47},
+        (Rectangle){310, 843, 55, 47},
+        (Rectangle){242, 843, 55, 47},
+        (Rectangle){175, 843, 55, 47},
+    },7);
+    inim1Anim_walkingLeft = CreateSpriteAnimation(inimigo1SpriteSheet, 15, (Rectangle[]){
+        (Rectangle){14, 88, 55, 47},
+        (Rectangle){80, 88, 55, 47},
+        (Rectangle){147, 88, 55, 47},
+        (Rectangle){207, 88, 55, 47},
+        (Rectangle){272, 88, 55, 47},
+        (Rectangle){347, 88, 55, 47},
+        (Rectangle){412, 88, 55, 47},
+    },7);
+    inim1Anim_attackingRight = CreateSpriteAnimation(inimigo1SpriteSheet, 18, (Rectangle[]){
+        (Rectangle){567, 1110, 55, 47},
+        (Rectangle){507, 1110, 55, 47},
+        (Rectangle){447, 1104, 55, 52},
+        (Rectangle){391, 1104, 55, 52},
+        (Rectangle){336, 1104, 55, 52},
+        (Rectangle){264, 1109, 64, 47},
+        (Rectangle){172,1109, 80, 47},
+        (Rectangle){86, 1109, 80, 47},
+        (Rectangle){6, 1109, 80, 47},
+        (Rectangle){567, 1168, 60, 47},
+        (Rectangle){504, 1168, 60, 47},
+        (Rectangle){440, 1168, 60, 47},
+        (Rectangle){380, 1168, 60, 47}
+    },13);
+    inim1Anim_attackingLeft = CreateSpriteAnimation(inimigo1SpriteSheet, 15, (Rectangle[]){
+        (Rectangle){21, 292, 55, 47},
+        (Rectangle){81, 292, 55, 47},
+        (Rectangle){141, 286, 55, 52},
+        (Rectangle){198, 286, 55, 52},
+        (Rectangle){249, 286, 55, 52},
+        (Rectangle){316, 291, 64, 47},
+        (Rectangle){391,291, 80, 47},
+        (Rectangle){477, 291, 80, 47},
+        (Rectangle){561, 291, 80, 47},
+        (Rectangle){21, 350, 60, 47},
+        (Rectangle){84, 350, 60, 47},
+        (Rectangle){148, 350, 60, 47},
+        (Rectangle){208, 350, 60, 47}
+    },13);
+    inim1Anim_dyingLeft = CreateSpriteAnimation(inimigo1SpriteSheet, 10,(Rectangle[]){
+        (Rectangle){15, 556, 55, 47},
+        (Rectangle){81, 556, 55, 47},
+        (Rectangle){148, 556, 55, 47},
+        (Rectangle){294, 556, 60, 47},
+        (Rectangle){371, 556, 65, 47},
+        (Rectangle){457, 556, 65, 47},
+        (Rectangle){17, 623, 65, 50},
+        (Rectangle){98, 623, 65, 50},
+        (Rectangle){184, 623, 65, 50},
+        (Rectangle){263, 623, 65, 50},
+        (Rectangle){345, 623, 65, 50},
+        (Rectangle){434, 623, 65, 50},
+        (Rectangle){14, 683, 68, 55},
+        (Rectangle){91, 683, 68, 55},
+        (Rectangle){178, 683, 68, 55},
+        (Rectangle){266, 683, 68, 55},
+        (Rectangle){339, 683, 68, 55},
+        (Rectangle){426, 683, 68, 55},
+        (Rectangle){510, 683, 68, 55},
+        (Rectangle){23, 750, 55, 55},
+        (Rectangle){89, 750, 55, 55},
+        (Rectangle){156, 750, 55, 55},
+        (Rectangle){210, 750, 50, 55},
+        (Rectangle){258, 750, 40, 55},
+        (Rectangle){298, 750, 40, 55}
+    },26);
+    inim1Anim_dyingRight = CreateSpriteAnimation(inimigo1SpriteSheet, 10, (Rectangle[]){
+        (Rectangle){573, 1373, 55, 47},
+        (Rectangle){507, 1373, 55, 47},
+        (Rectangle){440, 1373, 55, 47},
+        (Rectangle){365, 1373, 60, 47},
+        (Rectangle){289, 1373, 65, 47},
+        (Rectangle){207, 1373, 65, 47},
+        (Rectangle){122, 1373, 65, 47},
+        (Rectangle){560, 1440, 65, 50},
+        (Rectangle){478, 1440, 65, 50},
+        (Rectangle){394, 1440, 65, 50},
+        (Rectangle){316, 1440, 65, 50},
+        (Rectangle){232, 1440, 65, 50},
+        (Rectangle){144, 1440, 65, 50},
+        (Rectangle){561, 1500, 68, 55},
+        (Rectangle){484, 1500, 68, 55},
+        (Rectangle){395, 1500, 68, 55},
+        (Rectangle){309, 1500, 68, 55},
+        (Rectangle){235, 1500, 68, 55},
+        (Rectangle){148, 1500, 68, 55},
+        (Rectangle){65, 1500, 68, 55},
+        (Rectangle){565, 1567, 55, 55},
+        (Rectangle){499, 1567, 55, 55},
+        (Rectangle){432, 1567, 55, 55},
+        (Rectangle){384, 1567, 50, 55},
+        (Rectangle){346, 1567, 40, 55},
+        (Rectangle){306, 1567, 40, 55},
+    },26);
+    //  Inimigo 2
+    //  Inimigo 3
+    //----------------------------------------------------------------------------------
 
 
     #if defined(PLATFORM_WEB)
