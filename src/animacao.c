6f33c1c11f3862193fb8ffa32f9b08d0b5f0d02e
jaubertgualberto <jaubertgouv@gmail.com>
2024-02-27 23:18:23 -0300

    Mudanca de Arquivos e Adicionar Makefile

--- src/animacao.c
+++ src/animacao.c
@@ -1,37 +1,5 @@
-#include <stdio.h>
+#include "../include/animacao.h"
 
-#include "raylib.h"
-
-#include "movimentacao.h"
-#include "animacao.h"
-
-#define BOTAOINICIAL_POS_X (1280 - botaoStart.width) / 2
-#define BOTAOINICIAL_POS_Y (720 - botaoStart.height) / 2
-#define PLAYER_POSINICIAL_X 105
-#define PLAYER_POSINICIAL_Y 1205
-#define PLAYER_DIM_X 105
-#define PLAYER_DIM_Y 105
-
-
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
-
-//----------------------------------------------------------------------------------
-// Variáveis Locais (local para este módulo)
-//----------------------------------------------------------------------------------
-
-Texture2D playerSpriteSheet;
-Texture2D inimigo1SpriteSheet;
-Texture2D inimigo2SpriteSheet;
-Texture2D inimigo3SpriteSheet;
-Texture2D cenario;
-Texture2D cenarioLog;
-Texture2D botaoStart;
-
-//----------------------------------------------------------------------------------
-//  Animacoes
-// Player
 SpriteAnimation playerAnim_walkingRight;
 SpriteAnimation playerAnim_walkingLeft;
 SpriteAnimation playerAnim_idleRight;
@@ -66,51 +34,52 @@ SpriteAnimation inim3Anim_attackingRight;
 SpriteAnimation inim3Anim_attackingLeft;
 SpriteAnimation inim3Anim_dyingRight;
 SpriteAnimation inim3Anim_dyingLeft;
-//----------------------------------------------------------------------------------
 
-
-
-Rectangle botaoStartColis;
-Rectangle playerRect;
-
-float velY = 0;
-float jumpSpeed = 200.0f;
-float gravidade = 2.8f;
-int playerDirec;
-
-bool menu_open = true;
-bool isJumping = false; 
-bool jump = true;
-
-//----------------------------------------------------------------------------------
-// Declaração de Funções Locais
-//----------------------------------------------------------------------------------
-
-static void UpdateDrawFrame(void);
-
-//----------------------------------------------------------------------------------
-// Ponto de Entrada Principal
-//----------------------------------------------------------------------------------
-
-int main()
+SpriteAnimation CreateSpriteAnimation(Texture2D spriteSheet, int framesPerSecond, Rectangle rectangles[], int length)
 {
-    // Inicialização
-    InitWindow(1280, 720, "Tela Inicial");
+    SpriteAnimation spriteAnimation = 
+    {
+        .spriteSheet = spriteSheet,
+        .framesPerSecond = framesPerSecond,
+        .timeStarted = GetTime(),
+        .rectanges = NULL,
+        .rectanglesLength = length,
+    };
 
-    playerSpriteSheet = LoadTexture("./resources/Personagem_SpriteSheet.png");
-    inimigo1SpriteSheet = LoadTexture("resources/Inimigo1_SpriteSheet.png");
-    inimigo2SpriteSheet = LoadTexture("resources/Inimigo2_SpriteSheet.png");
-    inimigo3SpriteSheet = LoadTexture("resources/Inimigo3_SpriteSheet.png");
-    cenario = LoadTexture("./resources/cenario2.png");
-    cenarioLog = LoadTexture("./resources/cenario0.png");
-    botaoStart = LoadTexture("./resources/botao2.png");
+    //aloca/verifica a alocacao do espaco para a quantidade de retangulo
+    //caso seja possivel, designa espaco da memoria para a quantidade 
+    //de retangulos e salva o endereco do inicio em um ponteiro
+    Rectangle* mem = (Rectangle*)malloc(sizeof(Rectangle) * length); 
+    if (mem == NULL)
+    {
+        //não possui memoria
+        spriteAnimation.rectanglesLength = 0;
+        exit(1);
+        return spriteAnimation;
+    }
+    //atribui o endereço que foi guardado para a lista de retangulos
+    spriteAnimation.rectanges = mem;
 
-    botaoStartColis = (Rectangle){ BOTAOINICIAL_POS_X, BOTAOINICIAL_POS_Y, botaoStart.width, botaoStart.height };
-    playerRect = (Rectangle){PLAYER_POSINICIAL_X, PLAYER_POSINICIAL_Y, PLAYER_DIM_X, PLAYER_DIM_Y};
+    //preenche o array com os retangulos
+    for (int i = 0; i < length; i++) spriteAnimation.rectanges[i] = rectangles[i];
 
-    //----------------------------------------------------------------------------------
-    //  Animacoes
-    //  Player
+    return spriteAnimation;
+}
+
+void DisposeSpriteAnimation(SpriteAnimation animation)
+{
+    free(animation.rectanges);
+}
+
+void DrawSpriteAnimationPro(SpriteAnimation animation, Rectangle dest, Vector2 origin, float rotation, Color tint)//animacao de sprite, posicao que ira ficar na tela, onde eh o meio do retangulo, rotacao, matiz   
+{
+    int index = (int)((GetTime()- animation.timeStarted) *animation.framesPerSecond) % animation.rectanglesLength;
+
+    Rectangle source = animation.rectanges[index];
+    DrawTexturePro(animation.spriteSheet, source, dest, origin, rotation, tint);
+}
+
+void initAnimations(Texture2D playerSpriteSheet, Texture2D inimigo1SpriteSheet, Texture2D inimigo2SpriteSheet, Texture2D inimigo3SpriteSheet) {
     playerAnim_walkingRight = CreateSpriteAnimation(playerSpriteSheet,15,(Rectangle[]){
         (Rectangle){0,  71, 40, 44},
         (Rectangle){40, 71, 40, 44},
@@ -331,73 +300,11 @@ int main()
         (Rectangle){346, 1567, 40, 55},
         (Rectangle){306, 1567, 40, 55},
     },26);
-    //  Inimigo 2
-    //  Inimigo 3
-    //----------------------------------------------------------------------------------
-
-
-    #if defined(PLATFORM_WEB)
-        emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
-    #else
-        SetTargetFPS(60);
-        // Loop principal do jogo
-        while (!WindowShouldClose())
-        {
-            UpdateDrawFrame();
-        }
-    #endif
-
-    // Desinicialização
-    CloseWindow();
-    UnloadTexture(playerSpriteSheet);
-    UnloadTexture(cenario);
-    UnloadTexture(botaoStart);
-    UnloadTexture(cenarioLog);
-    DisposeSpriteAnimation(playerAnim_idleLeft);
-    DisposeSpriteAnimation(playerAnim_idleRight);
-    DisposeSpriteAnimation(playerAnim_walkingLeft);
-    DisposeSpriteAnimation(playerAnim_walkingRight);
-
-    return 0;
 }
 
-// Atualiza e desenha um frame do jogo
-static void UpdateDrawFrame(void)
-{
-    // Atualização
-    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(GetMousePosition(), botaoStartColis)) menu_open = false;
-    else
-    {
-        playerRect.x = movx(playerRect.x);
-        playerRect.y = movy(playerRect.y, gravidade);
 
-        if(IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) playerDirec = -1;
-        if(IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) playerDirec = 1;
-
-        if((IsKeyPressed(KEY_SPACE) && (playerRect.y >= (600 - playerRect.height))) || isJumping){
-            isJumping = true;
-            velY = jumpSpeed;
-            playerRect.y = pulo(playerRect.y, &velY, gravidade, playerRect.height, GetFrameTime(), &isJumping, &jump);
-        }   
-
-        if (playerRect.y > 600 ) playerRect.y = 600;
-    }
-
-    // Desenho
-    if (menu_open)
-    {
-        BeginDrawing();
-            ClearBackground(WHITE);
-            DrawTexture(cenarioLog, 0, 0, WHITE);
-            DrawTexture(botaoStart, BOTAOINICIAL_POS_X, BOTAOINICIAL_POS_Y, WHITE);
-        EndDrawing();
-    }
-    else
-    {
-        BeginDrawing();
-            ClearBackground(WHITE);
-            DrawTexture(cenario, 0, 0, WHITE);
-            //animacao
+void playerAnimation(int playerDirec, Rectangle playerRect) {
+    //animacao
             switch (playerDirec)
             {
             case 1:
@@ -430,6 +337,4 @@ static void UpdateDrawFrame(void)
                 
                 break;
             }
-        EndDrawing();
-    }
 }
